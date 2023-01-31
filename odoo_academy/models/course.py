ce6d3f26d6c4d6d44605d77776cddf53a11875b0
prointec <jrichmond@prointeccr.com>
2023-01-31 03:02:25 +0000

    academy orm basic

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -1,6 +1,7 @@
 # -*- coding:utf-8 -*-
 
 from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError
 
 class Course(models.Model):
     
@@ -17,3 +18,20 @@ class Course(models.Model):
                              copy = False)
     
     active = fields.Boolean(string='Active', default='True')
+    
+    base_price = fields.Float(string='Base Price', default=0.00)
+    addtional_fee = fields.Float(string='Additional Fee', default=0.00)
+    total_price = fields.Float(string='Total Price', read_only=True)
+    
+    @api.onchange('base_price','addtional_fee')
+    def _onchange_total_price(self):
+        if self.base_price < 0.00:
+            raise UserError('Base Price cannot be set as Negative')
+        self.total_price = self.base_price + self.addtional_fee
+        
+    @api.constrains('addtional_fee')
+    def _check_addtional_fee(self):
+        for record in self:
+            if record.addtional_fee < 10.00:
+                raise ValidationError('Addtional fee cannot be less than 10.00 %s' % record.addtional_fee)
+        
---
+++ odoo_academy/models/session.py
@@ -0,0 +1,15 @@
+# -*- coding:utf-8 -*-
+
+from odoo import models, fields, api
+
+class Session(models.Model):
+    
+    _name = 'academy.session'
+    _description = 'Session Info'
+    
+    course_id = fields.Many2one(comodel_name='academy.course',
+                               string='Course',
+                               ondelete='cascade',
+                               required=True)
+    
+    name = fields.Char(string='Title', related='course_id.name')
